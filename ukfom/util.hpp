a573c8012bc42f09a09309b41f3fba269b1ed6b6
Lashika Medagoda <Lashika.Medagoda@dfki.de>
2017-05-30 15:46:58 +0200

    added mahalanobis tests for 2dof case

--- ukfom/util.hpp
+++ ukfom/util.hpp
@@ -44,6 +44,32 @@ static bool accept_any_mahalanobis_distance(const scalar_type &mahalanobis2)
 	return true;
 }
 
+template <typename scalar_type>
+static bool d2p99(const scalar_type &mahalanobis2)
+{
+	if(mahalanobis2>9.21) // for 2 degrees of freedom, 99% likelihood = 9.21, https://www.uam.es/personal_pdi/ciencias/anabz/Prest/Trabajos/Critical_Values_of_the_Chi-Squared_Distribution.pdf
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+template <typename scalar_type>
+static bool d2p95(const scalar_type &mahalanobis2)
+{
+	if(mahalanobis2>5.991) // for 2 degrees of freedom, 95% likelihood = 5.991, https://www.uam.es/personal_pdi/ciencias/anabz/Prest/Trabajos/Critical_Values_of_the_Chi-Squared_Distribution.pdf
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
 template<typename T>
 static T
 id(const T &x)
